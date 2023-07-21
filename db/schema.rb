9ebb1d4d71582523a8319d85e61f582619d148d1
AJCodes1 <etsncamacho@gmail.com>
2023-07-21 11:27:20 -0700

    fixed database structure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_205415) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_170239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_205415) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
